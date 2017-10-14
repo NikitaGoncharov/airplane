3ba5cabf9b147ead9c424d41d7200ea2b4ebefca
NikitaGoncharov <verinji@gmail.com>
2017-10-14 17:38:22 +0300

    Airplane Project

---
+++ air22/air22/Airplane.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace air22
+{
+    class Airplane
+    {
+        static Airplane()
+        {
+            MinAltitudeAuto = 2000;
+            MaxAltitudeAuto = 10000;
+        }
+        public int Capacity { get; private set; }
+        public bool AutoPilotOn { get; set; }
+
+        /// <summary>
+        /// Fuel consuption. kg/km
+        /// </summary>
+        public float Consuption { get; private set; }
+
+        public int Altitude { get; private set; }
+
+
+        public static decimal TicketPrice { get; set; }
+        public static int MinAltitudeAuto { get; set; }
+        public static int MaxAltitudeAuto { get; set; }
+
+        private int _altitudeIncrement;
+
+        public Airplane(int capacity, float consuption, int altitudeIncrement)
+        {
+            Altitude = 1200;
+            AutoPilotOn = false;
+            Capacity = capacity;
+            Consuption = consuption;
+            _altitudeIncrement = altitudeIncrement;
+        }
+
+        public int Climb(int increment)
+        {
+            if (!AutoPilotOn) return Altitude += increment;
+
+            if (Altitude + increment < MaxAltitudeAuto)
+            {
+                return Altitude += increment;
+            }
+
+            return Altitude = MaxAltitudeAuto;
+        }
+
+        public int Down(int increment)
+        {
+            if (AutoPilotOn)
+            {
+                if (Altitude - increment > MinAltitudeAuto)
+                {
+                    return Altitude -= increment;
+                }
+                if (Altitude < MinAltitudeAuto) return Altitude;
+                return Altitude = MinAltitudeAuto;
+            }
+
+            if (Altitude - increment < 0) return Altitude = 0;
+            return Altitude -= increment;
+        }
+
+        public void SetAltitude(int targetAlitude)
+        {
+            if (AutoPilotOn)
+            {
+                if (targetAlitude > MaxAltitudeAuto || targetAlitude < MinAltitudeAuto)
+                {
+                    if (Altitude < targetAlitude) Altitude = MaxAltitudeAuto;
+                    else if (Altitude > targetAlitude) Altitude = MinAltitudeAuto;
+                    else Altitude = Altitude;
+                }
+                else
+                {
+                    if (Altitude < targetAlitude) Altitude = Climb(targetAlitude - Altitude);
+                    else if (Altitude > targetAlitude) Altitude = Down(Altitude - targetAlitude);
+                    else Altitude = Altitude;
+                }
+            }
+            else
+            {
+                if (Altitude < targetAlitude) Altitude = Climb(targetAlitude - Altitude);
+                else if (Altitude > targetAlitude) Altitude = Down(Altitude - targetAlitude);
+                else Altitude = Altitude;
+            }
+        }
+    }
+}
---
+++ air22/air22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace air22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App started");
+            Airplane airplane = new Airplane(140, 2.26F, 100);
+            int a;
+            a = int.Parse(Console.ReadLine());
+            airplane.AutoPilotOn = true;
+            airplane.SetAltitude(a);
+            Console.WriteLine(airplane.Altitude);
+
+            Console.ReadLine();
+        }
+    }
+}
